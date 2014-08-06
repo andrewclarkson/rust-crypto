e2954163785c3008a981ffb4d359c55b66654931
Andrew Clarkson <andrewclarkson@live.com>
2014-08-06 15:14:01 -0500

    Working modular exponentiation and tests

--- src/rust-crypto/prime_rng.rs
+++ src/rust-crypto/prime_rng.rs
@@ -5,83 +5,33 @@ use std::result::Result;
 use std::rand::{task_rng, Rng, TaskRng};
 use num::bigint::{ToBigUint, BigUint, RandBigInt};
 use num::integer::Integer;
-/*
- * A prime number generator useful in generating RSA keys
- */
-struct PrimeRng<'a> {
-    generator: &'a mut RandBigInt
-}
-
-impl <'a>PrimeRng<'a> {
-    pub fn new() -> PrimeRng<'a> {
-        PrimeRng {
-            generator: &mut task_rng()
-        }
-    }
-
-    pub fn from_rng<T: RandBigInt>(rng: &'a mut T) -> PrimeRng<'a> {
-        PrimeRng {
-            generator: rng  
-        }
-    }
-    /*
-     * Generates a prime number 'bit_size' number of bits in length 
-     */
-    pub fn generate(&self, bit_size: uint) -> Result<BigUint, &'static str> {
-    
-        /*
-         * Generate an unsigned candidate number using the standard
-         * library random number generator.
-         */
-        let candidate = self.generator.gen_biguint(bit_size);
-    
-        
-    
-    
-    }
-}
 
 /*
- * Implements the Miller-Rabin probablistic primality test.
- *
- *
- */
-pub fn probably_prime(candidate: BigUint, security: uint) -> bool {
-
-    if candidate <= 1u.to_biguint().unwrap() {
-        return false;
-    }
-
-    if candidate == 2u.to_biguint().unwrap() {
-        return true;
-    }
-
-    if candidate.is_even() {
-        return false;
-    }
-
-    
-        
-
-}
-
-
-/*
- *
  * Raises 
  *
  */
-pub fn modular_exp(base: BigUint, exponent: BigUint, modulus: BigUint) -> BigUint {
+pub fn modular_exp(mut base: BigUint, mut exponent: BigUint, modulus: BigUint) -> BigUint {
 
-    let result = 1u.to_biguint();
-    let base = base.div()
 
-    while power > 0 {
-        if exponent.mod(2) == 1 {
-            result = (result * base).mod(modulus);
+
+    let zero = 0u.to_biguint().unwrap();
+    
+    assert!(modulus != zero);
+    
+    let one = 1u.to_biguint().unwrap();
+    let two = 2u.to_biguint().unwrap();
+
+
+    let mut result = 1u.to_biguint().unwrap();
+    base = base % modulus;
+    exponent = exponent;
+
+    while exponent > zero {
+        if exponent % two == one {
+            result = (result * base) % modulus;
         }
         exponent = exponent >> 1;
-        base = (base * base).mod(modulus);
+        base = (base * base) % modulus;
     }
     result
 }
@@ -90,17 +40,93 @@ pub fn modular_exp(base: BigUint, exponent: BigUint, modulus: BigUint) -> BigUin
 #[cfg(test)]
 mod test {
 
-    use super::probably_prime;
-    use num::bigint::ToBigUint;
+    use super::modular_exp;
+    use num::bigint::{ToBigUint};
 
     #[test]
-    fn test_definitely_prime() {
-        assert!(probably_prime(17u.to_biguint().unwrap(), 1));
+    #[should_fail]
+    fn test_modular_exp_bad() {
+        assert_eq!(
+            modular_exp(
+                26614u.to_biguint().unwrap(),
+                480u.to_biguint().unwrap(),
+                0u.to_biguint().unwrap()
+            ), 
+            15120u.to_biguint().unwrap()
+        );
+        
     }
 
     #[test]
     fn test_modular_exp() {
+
+        /*
+         * The wikipedia example
+         */
+        assert_eq!(
+            modular_exp(
+                4u.to_biguint().unwrap(),
+                13u.to_biguint().unwrap(),
+                497u.to_biguint().unwrap()
+            ), 
+            445u.to_biguint().unwrap()
+        );
+
+        /*
+         * Zero's 
+         */
+        assert_eq!(
+            modular_exp(
+                0u.to_biguint().unwrap(),
+                25871u.to_biguint().unwrap(),
+                14932u.to_biguint().unwrap()
+            ), 
+            0u.to_biguint().unwrap()
+        );
+
+        assert_eq!(
+            modular_exp(
+                962u.to_biguint().unwrap(),
+                0u.to_biguint().unwrap(),
+                29008u.to_biguint().unwrap()
+            ), 
+            1u.to_biguint().unwrap()
+        );
         
+        /*
+         * Random examples calculated using python
+         */
+        assert_eq!(
+            modular_exp(
+                6826u.to_biguint().unwrap(),
+                25871u.to_biguint().unwrap(),
+                14932u.to_biguint().unwrap()
+            ), 
+            2632u.to_biguint().unwrap()
+        );
+        
+        assert_eq!(
+            modular_exp(
+                962u.to_biguint().unwrap(),
+                6431u.to_biguint().unwrap(),
+                29008u.to_biguint().unwrap()
+            ), 
+            10064u.to_biguint().unwrap()
+        );
+        
+        assert_eq!(
+            modular_exp(
+                26614u.to_biguint().unwrap(),
+                480u.to_biguint().unwrap(),
+                18928u.to_biguint().unwrap()
+            ), 
+            15120u.to_biguint().unwrap()
+        );
+
+
+
+
+
     }
 
 }
